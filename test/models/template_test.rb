8632dcd17026a66cb04605db9c661cad8a3deb87
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2025-12-25 16:29:55 +0900

    test: テンプレートテストの整理

--- test/models/template_test.rb
+++ test/models/template_test.rb
@@ -1,27 +1,27 @@
 require "test_helper"
 
 class TemplateTest < ActiveSupport::TestCase
+  setup do
+    @template = templates(:moving)
+  end
+
   test "フィクスチャのテンプレートは有効" do
-    assert templates(:moving).valid?
+    assert @template.valid?
   end
 
   test "タイトルは120文字以内" do
-    template = templates(:moving)
-    template.title = "a" * 121
+    @template.title = "a" * 121
 
-    assert_not template.valid?
+    assert_not @template.valid?
   end
 
   test "author_notesは500文字以内" do
-    template = templates(:moving)
-    template.author_notes = "a" * 501
+    @template.author_notes = "a" * 501
 
-    assert_not template.valid?
+    assert_not @template.valid?
   end
 
   test "average_ratingは平均値を返す" do
-    template = templates(:moving)
-
-    assert_in_delta 5.0, template.average_rating, 0.001
+    assert_in_delta 5.0, @template.average_rating, 0.001
   end
 end
